67c5cf49d04bd085ba3ce130991fa9bdf8125673
iwabuchiken <iwabuchi.k.2010@gmail.com>
2012-04-24 11:13:20 +0900

    BV=0.0.2
    [d]
    1. UI => complete
    
    [n]
    1. Add: Listeners

--- gen/myapp/homebase2/R.java
+++ gen/myapp/homebase2/R.java
@@ -18,17 +18,20 @@ public final class R {
         public static final int t1_LL1_tv1=0x7f050003;
         public static final int t1_LL2_btn1=0x7f050005;
         public static final int t1_LL2_tv1=0x7f050006;
-        public static final int t2_LL1_btn1=0x7f050009;
-        public static final int t2_LL1_tv1=0x7f05000a;
-        public static final int t3_LL1_btn1=0x7f05000d;
-        public static final int t3_LL1_tv1=0x7f05000e;
+        public static final int t1_LL3_btn1=0x7f050008;
+        public static final int t1_LL3_tv1=0x7f050009;
+        public static final int t2_LL1_btn1=0x7f05000c;
+        public static final int t2_LL1_tv1=0x7f05000d;
+        public static final int t3_LL1_btn1=0x7f050010;
+        public static final int t3_LL1_tv1=0x7f050011;
         public static final int tab1=0x7f050000;
         public static final int tab1_LL1=0x7f050001;
         public static final int tab1_LL2=0x7f050004;
-        public static final int tab2=0x7f050007;
-        public static final int tab2_LL1=0x7f050008;
-        public static final int tab3=0x7f05000b;
-        public static final int tab3_LL1=0x7f05000c;
+        public static final int tab1_LL3=0x7f050007;
+        public static final int tab2=0x7f05000a;
+        public static final int tab2_LL1=0x7f05000b;
+        public static final int tab3=0x7f05000e;
+        public static final int tab3_LL1=0x7f05000f;
     }
     public static final class layout {
         public static final int homebase=0x7f030000;
@@ -36,12 +39,21 @@ public final class R {
     }
     public static final class string {
         public static final int app_name=0x7f040001;
-        public static final int default_text=0x7f040006;
-        public static final int exec_btn=0x7f040005;
+        public static final int default_text=0x7f040009;
+        /**  Global 
+         */
+        public static final int exec_btn=0x7f040008;
         public static final int hello=0x7f040000;
-        public static final int t2_LL1_btn1=0x7f040004;
+        /**  Tab 2     
+         */
+        public static final int t2_LL1_btn1=0x7f040007;
+        /**  Tab 1 
+         */
         public static final int tab1_LL1_btn1=0x7f040002;
         public static final int tab1_LL2_btn1=0x7f040003;
-        public static final int tv_size_1=0x7f040007;
+        public static final int tab1_LL3_btn1=0x7f040005;
+        public static final int tv_size_1=0x7f04000a;
+        public static final int txt_t1_LL2_tv1=0x7f040004;
+        public static final int txt_t1_LL3_tv1=0x7f040006;
     }
 }
