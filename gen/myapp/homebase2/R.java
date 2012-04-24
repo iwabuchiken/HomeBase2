0b92e5d8e24fa898369defebcb23129bfaf015f3
iwabuchiken <iwabuchi.k.2010@gmail.com>
2012-04-25 08:36:22 +0900

    BV=0.0.1, f2_upload-image-files_#5
    [d]
    1. Complete => homebase.xml
    
    [n]
    1. Code logic part of the feature

--- gen/myapp/homebase2/R.java
+++ gen/myapp/homebase2/R.java
@@ -11,7 +11,8 @@ public final class R {
     public static final class attr {
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
+        public static final int border=0x7f020000;
+        public static final int ic_launcher=0x7f020001;
     }
     public static final class id {
         public static final int t1_LL1_btn1=0x7f050002;
@@ -21,17 +22,18 @@ public final class R {
         public static final int t1_LL3_btn1=0x7f050008;
         public static final int t1_LL3_tv1=0x7f050009;
         public static final int t2_LL1_btn1=0x7f05000c;
-        public static final int t2_LL1_tv1=0x7f05000d;
-        public static final int t3_LL1_btn1=0x7f050010;
-        public static final int t3_LL1_tv1=0x7f050011;
+        public static final int t2_LL1_btn2=0x7f05000d;
+        public static final int t2_iv=0x7f05000e;
+        public static final int t3_LL1_btn1=0x7f050011;
+        public static final int t3_LL1_tv1=0x7f050012;
         public static final int tab1=0x7f050000;
         public static final int tab1_LL1=0x7f050001;
         public static final int tab1_LL2=0x7f050004;
         public static final int tab1_LL3=0x7f050007;
         public static final int tab2=0x7f05000a;
         public static final int tab2_LL1=0x7f05000b;
-        public static final int tab3=0x7f05000e;
-        public static final int tab3_LL1=0x7f05000f;
+        public static final int tab3=0x7f05000f;
+        public static final int tab3_LL1=0x7f050010;
     }
     public static final class layout {
         public static final int homebase=0x7f030000;
@@ -39,10 +41,10 @@ public final class R {
     }
     public static final class string {
         public static final int app_name=0x7f040001;
-        public static final int default_text=0x7f04000d;
+        public static final int default_text=0x7f04000e;
         /**  Global 
          */
-        public static final int exec_btn=0x7f04000c;
+        public static final int exec_btn=0x7f04000d;
         public static final int hello=0x7f040000;
         public static final int password_type_alnum=0x7f04000a;
         /**  Selection items: Type 
@@ -52,13 +54,14 @@ public final class R {
         /**  Tab 2     
          */
         public static final int t2_LL1_btn1=0x7f04000b;
+        public static final int t2_LL1_btn2=0x7f04000c;
         /**  Tab 1 
  Button labels 
          */
         public static final int tab1_LL1_btn1=0x7f040002;
         public static final int tab1_LL2_btn1=0x7f040003;
         public static final int tab1_LL3_btn1=0x7f040004;
-        public static final int tv_size_1=0x7f04000e;
+        public static final int tv_size_1=0x7f04000f;
         /**  TextView labels 
          */
         public static final int txt_t1_LL1_tv1=0x7f040005;
