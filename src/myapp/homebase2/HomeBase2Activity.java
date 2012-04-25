e870b246c145a292781ba4ab3c3c8a51da784e96
iwabuchiken <iwabuchi.k.2010@gmail.com>
2012-04-25 09:03:22 +0900

    B=#21 v=0.0.2
    [d]
    1. Listeners added
    2. Display toasts

--- src/myapp/homebase2/HomeBase2Activity.java
+++ src/myapp/homebase2/HomeBase2Activity.java
@@ -3,7 +3,10 @@ package myapp.homebase2;
 import android.app.Activity;
 import android.app.TabActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class HomeBase2Activity extends TabActivity {
     /** Called when the activity is first created. */
@@ -13,6 +16,10 @@ public class HomeBase2Activity extends TabActivity {
         
         // create tabs
         TabUtils.createTabsN(this, 3);
+        
+        /* Tab 1
+         * 
+         */
         
         // Set a listener to the button
         Button t1_LL1_btn1 = (Button) findViewById(R.id.t1_LL1_btn1);
@@ -28,5 +35,35 @@ public class HomeBase2Activity extends TabActivity {
         Button t1_LL3_btn1 = (Button) findViewById(R.id.t1_LL3_btn1);
         t1_LL3_btn1.setTag("setLength");
         t1_LL3_btn1.setOnClickListener(new ButtonClickListener(this));
-    }
+        
+        /* Tab 2
+         * 
+         */
+        // Set listener: "Choose"
+        Button t2_LL1_btn1 = (Button) findViewById(R.id.t2_LL1_btn1);
+        t2_LL1_btn1.setTag("choseFiles");
+        t2_LL1_btn1.setOnClickListener(new OnClickListener(){
+
+			public void onClick(View v) {
+				// 
+				// debug
+				Toast.makeText(HomeBase2Activity.this, "Choose files", Toast.LENGTH_SHORT).show();
+			}
+        	
+        });//t2_LL1_btn1.setOnClickListener()
+        
+        // Set listener: "Upload"
+        Button t2_LL1_btn2 = (Button) findViewById(R.id.t2_LL1_btn2);
+        t2_LL1_btn2.setTag("uploadFiles");
+        t2_LL1_btn2.setOnClickListener(new OnClickListener(){
+
+			public void onClick(View v) {
+				// 
+				// debug
+				Toast.makeText(HomeBase2Activity.this, "Upload files", Toast.LENGTH_SHORT).show();
+			}
+        	
+        });//t2_LL1_btn1.setOnClickListener()
+        
+    }//public void onCreate(Bundle savedInstanceState)
 }
